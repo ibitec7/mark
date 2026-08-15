58c3ec6ae362c7e7bd744e593ec78b4aab156451
ibitec7 <syed.ibrahim.omer.2@gmail.com>
2026-08-15 20:18:36 +0500

    backup: sync

---
+++ proofs/.lake/build/ir/Proofs/OperatorIdentifiability.c
@@ -0,0 +1,224 @@
+// Lean compiler output
+// Module: Proofs.OperatorIdentifiability
+// Imports: public import Init public import Mathlib
+#include <lean/lean.h>
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-label"
+#elif defined(__GNUC__) && !defined(__CLANG__)
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-label"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+static lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__0;
+lean_object* lp_mathlib___private_Mathlib_Data_Real_Basic_0__Real_mul(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__2(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator___redArg(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+lean_object* lp_mathlib_Matrix_nonUnitalNonAssocSemiring___redArg(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__3___boxed(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+lean_object* lp_mathlib_dotProduct___redArg(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__2___boxed(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__1(lean_object*, lean_object*, lean_object*, lean_object*);
+extern lean_object* lp_mathlib_Real_instAddCommMonoid;
+lean_object* lp_mathlib_npowBinRec_go___redArg(lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__3(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__0(lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator___redArg___lam__0(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+static lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__1;
+lean_object* lp_mathlib_Semiring_toNonAssocSemiring___redArg(lean_object*);
+extern lean_object* lp_mathlib_Real_instCommSemiring;
+lean_object* lp_mathlib_Matrix_nonAssocSemiring___redArg(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__0(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4) {
+_start:
+{
+lean_object* x_5; 
+x_5 = lean_apply_3(x_1, x_2, x_3, x_4);
+return x_5;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__1(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4) {
+_start:
+{
+lean_object* x_5; 
+x_5 = lean_apply_3(x_1, x_2, x_4, x_3);
+return x_5;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__2(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6) {
+_start:
+{
+lean_object* x_7; lean_object* x_8; 
+x_7 = lp_mathlib_npowBinRec_go___redArg(x_1, x_2, x_3, x_4);
+x_8 = lean_apply_2(x_7, x_6, x_5);
+return x_8;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__2___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6) {
+_start:
+{
+lean_object* x_7; 
+x_7 = lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__2(x_1, x_2, x_3, x_4, x_5, x_6);
+lean_dec(x_2);
+return x_7;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__3(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6, lean_object* x_7, lean_object* x_8, lean_object* x_9) {
+_start:
+{
+lean_object* x_10; lean_object* x_11; 
+x_10 = lean_alloc_closure((void*)(lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__2___boxed), 6, 5);
+lean_closure_set(x_10, 0, x_1);
+lean_closure_set(x_10, 1, x_2);
+lean_closure_set(x_10, 2, x_3);
+lean_closure_set(x_10, 3, x_4);
+lean_closure_set(x_10, 4, x_9);
+x_11 = lp_mathlib_dotProduct___redArg(x_5, x_6, x_7, x_8, x_10);
+return x_11;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__3___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6, lean_object* x_7, lean_object* x_8, lean_object* x_9) {
+_start:
+{
+lean_object* x_10; 
+x_10 = lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__3(x_1, x_2, x_3, x_4, x_5, x_6, x_7, x_8, x_9);
+lean_dec_ref(x_7);
+return x_10;
+}
+}
+static lean_object* _init_lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__0() {
+_start:
+{
+lean_object* x_1; lean_object* x_2; 
+x_1 = lp_mathlib_Real_instCommSemiring;
+x_2 = lp_mathlib_Semiring_toNonAssocSemiring___redArg(x_1);
+return x_2;
+}
+}
+static lean_object* _init_lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__1() {
+_start:
+{
+lean_object* x_1; 
+x_1 = lean_alloc_closure((void*)(lp_mathlib___private_Mathlib_Data_Real_Basic_0__Real_mul), 2, 0);
+return x_1;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6, lean_object* x_7) {
+_start:
+{
+lean_object* x_8; lean_object* x_9; lean_object* x_10; lean_object* x_11; lean_object* x_12; lean_object* x_13; lean_object* x_14; lean_object* x_15; lean_object* x_16; lean_object* x_17; lean_object* x_18; lean_object* x_19; lean_object* x_20; lean_object* x_21; lean_object* x_22; lean_object* x_23; lean_object* x_24; 
+x_8 = lp_mathlib_Real_instAddCommMonoid;
+x_9 = lp_mathlib_Real_instCommSemiring;
+x_10 = lean_ctor_get(x_3, 0);
+lean_inc(x_10);
+x_11 = lean_ctor_get(x_3, 1);
+lean_inc(x_11);
+x_12 = lean_ctor_get(x_3, 2);
+lean_inc(x_12);
+lean_dec_ref(x_3);
+x_13 = lean_ctor_get(x_9, 0);
+lean_inc_ref(x_13);
+lean_inc(x_1);
+x_14 = lp_mathlib_Matrix_nonUnitalNonAssocSemiring___redArg(x_13, x_1);
+x_15 = lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__0;
+lean_inc(x_1);
+x_16 = lp_mathlib_Matrix_nonAssocSemiring___redArg(x_15, x_1, x_2);
+x_17 = lean_ctor_get(x_16, 1);
+lean_inc(x_17);
+lean_dec_ref(x_16);
+x_18 = lean_ctor_get(x_14, 1);
+lean_inc(x_18);
+lean_dec_ref(x_14);
+x_19 = lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__1;
+lean_inc(x_4);
+x_20 = lean_alloc_closure((void*)(lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__0), 4, 3);
+lean_closure_set(x_20, 0, x_12);
+lean_closure_set(x_20, 1, x_4);
+lean_closure_set(x_20, 2, x_6);
+lean_inc(x_4);
+x_21 = lean_alloc_closure((void*)(lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__1), 4, 3);
+lean_closure_set(x_21, 0, x_11);
+lean_closure_set(x_21, 1, x_4);
+lean_closure_set(x_21, 2, x_7);
+x_22 = lean_apply_1(x_10, x_4);
+lean_inc(x_1);
+x_23 = lean_alloc_closure((void*)(lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___lam__3___boxed), 9, 8);
+lean_closure_set(x_23, 0, x_18);
+lean_closure_set(x_23, 1, x_5);
+lean_closure_set(x_23, 2, x_17);
+lean_closure_set(x_23, 3, x_22);
+lean_closure_set(x_23, 4, x_1);
+lean_closure_set(x_23, 5, x_19);
+lean_closure_set(x_23, 6, x_8);
+lean_closure_set(x_23, 7, x_20);
+x_24 = lp_mathlib_dotProduct___redArg(x_1, x_19, x_8, x_23, x_21);
+return x_24;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_markovParameter(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6, lean_object* x_7, lean_object* x_8, lean_object* x_9) {
+_start:
+{
+lean_object* x_10; 
+x_10 = lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg(x_2, x_3, x_5, x_6, x_7, x_8, x_9);
+return x_10;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator___redArg___lam__0(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5, lean_object* x_6, lean_object* x_7) {
+_start:
+{
+lean_object* x_8; 
+x_8 = lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg(x_1, x_2, x_3, x_4, x_5, x_6, x_7);
+return x_8;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator___redArg(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; lean_object* x_5; lean_object* x_6; 
+x_4 = lean_ctor_get(x_3, 3);
+lean_inc(x_4);
+x_5 = lean_alloc_closure((void*)(lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator___redArg___lam__0), 7, 3);
+lean_closure_set(x_5, 0, x_1);
+lean_closure_set(x_5, 1, x_2);
+lean_closure_set(x_5, 2, x_3);
+x_6 = lean_alloc_ctor(0, 2, 0);
+lean_ctor_set(x_6, 0, x_4);
+lean_ctor_set(x_6, 1, x_5);
+return x_6;
+}
+}
+LEAN_EXPORT lean_object* lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5) {
+_start:
+{
+lean_object* x_6; 
+x_6 = lp_proofs_Proofs_OperatorIdentifiability_extendedMarkovOperator___redArg(x_2, x_3, x_5);
+return x_6;
+}
+}
+lean_object* initialize_Init(uint8_t builtin);
+lean_object* initialize_mathlib_Mathlib(uint8_t builtin);
+static bool _G_initialized = false;
+LEAN_EXPORT lean_object* initialize_proofs_Proofs_OperatorIdentifiability(uint8_t builtin) {
+lean_object * res;
+if (_G_initialized) return lean_io_result_mk_ok(lean_box(0));
+_G_initialized = true;
+res = initialize_Init(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_mathlib_Mathlib(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__0 = _init_lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__0();
+lean_mark_persistent(lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__0);
+lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__1 = _init_lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__1();
+lean_mark_persistent(lp_proofs_Proofs_OperatorIdentifiability_markovParameter___redArg___closed__1);
+return lean_io_result_mk_ok(lean_box(0));
+}
+#ifdef __cplusplus
+}
+#endif
